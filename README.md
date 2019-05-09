a3150e484d5ba5b6eebd387ec165f83f2c8376ff
S. Fischer <sfis@informatik.uni-kiel.de>
2019-05-09 12:57:01 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+symbol translator (symbotrans)
+==============================
+
+symbotrans is a web application (HTML5, CSS, JavaScript) to convert
+between symbols of multiple defined systems. A system can have multiple
+variations.
+
+Basically, systems and symbols and relation between these symbols have
+to be defined in a translation table file (comma-seperated UTF-8 file).
+
+This application was developed having translations between multiple
+phonetic transcription systems used in the danish language in mind.
+
---
+++ data/config.js
@@ -0,0 +1,36 @@
+/**
+ *
+ * javascript file that contains some configurations
+ *
+ */
+
+
+'use strict';
+
+const tabFileName = 'data/translationTable.csv';
+
+//                        true/on false/off
+const variationSwitch = [ "with", "without"];
+
+const title = "symbotrans";
+const copyrightAdditionalAuthors = "";
+
+const licenseName = "";
+
+// some default values
+let defaultInput = ["abc"];
+
+let defaultSrcSystem = 0;
+let defaultDstSystem = 1;
+let debugMode = false;
+
+// filter of translations
+let removeLongSrcParts = true;
+let removeDuplicateDstParts = true;
+let removeSmallerDstParts = true;
+
+let expertMode = false;
+let outputOnly = false;
+
+let input_limit = 18;
+
